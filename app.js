856566ddef71117441a989028211295627316648
dpomeran <dpomeran@student.touro.edu>
2018-12-18 10:24:30 -0500

    got login logic to work!!

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(flash());
 app.use(require('connect-flash')());
 app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
+  res.locals.user = req.user || null;
   next();
 });
 
